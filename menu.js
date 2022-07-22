cf09dfab458f4f4e474fb24d8359924235baef6e
michelecattellani <104779420+michelecattellani@users.noreply.github.com>
2022-07-22 17:21:35 +0200

    vabe

---
+++ menu.js
@@ -0,0 +1,43 @@
+let elements = document.querySelectorAll(".rolling-text");
+
+elements.forEach((element) => {
+    let innerText = element.innerText;
+    element.innerHTML = "";
+
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("block");
+
+    for (let letter of innerText) {
+        let span = document.createElement("span");
+        span.innerText = letter.trim() === "" ? "\xa0" : letter;
+        span.classList.add("letter");
+        textContainer.appendChild(span);
+    }
+
+    element.appendChild(textContainer);
+    element.appendChild(textContainer.cloneNode(true));
+});
+
+elements.forEach((element) => {
+
+    let currentImage = ''
+
+    element.addEventListener("mouseover", () => {
+        let imageIndex = element.dataset.image;
+        currentImage = document.getElementById(imageIndex)
+        currentImage.classList.add('current')
+        let tl = gsap.to(currentImage, {
+            duration: .4,
+            transform: 'translate(0)',
+            opacity: .8,
+        })
+    });
+
+    element.addEventListener('mouseout', (e) => {
+        let tl = gsap.to(currentImage, {
+            duration: .4,
+            transform: 'translate(0, 50px)',
+            opacity: 0
+        })
+    })
+});
